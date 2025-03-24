f441a908c21947d4c5f9c64b1ebc2b713e9c13a3
PwFaze <feze2013@hotmail.com>
2025-03-24 13:28:34 +0700

    fix: merge conflict

--- internal/dto/transaction.go
+++ internal/dto/transaction.go
@@ -3,10 +3,13 @@ package dto
 import (
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Transaction struct {
-	Type    int16 	  		   `json:"type"`
-	Amount  float64   		   `json:"amount"`
-	OrderID  primitive.ObjectID `json:"orderID,omitempty"`
-	Payment  string             `json:"payment"`
-	Date    time.Time            `json:"date"`
+	Type    int16              `json:"type"`
+	Amount  float64            `json:"amount"`
+	OrderID primitive.ObjectID `json:"orderID,omitempty"`
+	Payment string             `json:"payment"`
+	Date    time.Time          `json:"date"`
 }
--- internal/model/transaction.go
+++ internal/model/transaction.go
@@ -3,10 +3,13 @@ package model
 import (
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 type Transaction struct {
-	Type    int16	             `json:"type" bson:"type"`
-	Amount  float64   		   `json:"amount" bson:"amount" binding:"gte=0"`
-	OrderID  primitive.ObjectID `json:"orderID,omitempty" bson:"_id,omitempty"`
-	Payment  string             `json:"payment" bson:"payment"`
-	Date    time.Time            `json:"data" bson:"date"`
+	Type    int16              `json:"type" bson:"type"`
+	Amount  float64            `json:"amount" bson:"amount" binding:"gte=0"`
+	OrderID primitive.ObjectID `json:"orderID,omitempty" bson:"_id,omitempty"`
+	Payment string             `json:"payment" bson:"payment"`
+	Date    time.Time          `json:"data" bson:"date"`
 }
